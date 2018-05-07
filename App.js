6ceabeba5bdc684e756d8c7a0d0c91c1ea7239bb
Pedro Faria <pedrofaria1996@hotmail.com>
2018-05-07 22:57:52 +0100

    init leaflet, declare map

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ window.onload = function () {
 
     methods: {
       initMap: function () {
-        console.log('map')
         var map = L.map('map').setView([51.505, -0.09], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
