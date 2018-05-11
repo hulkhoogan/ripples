62253a638c2465863933235fdfae16bc6a8a4b72
Pedro Faria <pedrofaria1996@hotmail.com>
2018-05-11 02:04:52 +0100

    add toolbar to control maps

--- app/view/home/HomeController.js
+++ app/view/home/HomeController.js
@@ -1,5 +1,29 @@
 Ext.define('Ripples.view.home.HomeController', {
   extend: 'Ext.app.ViewController',
-  alias: 'controller.home'
+  alias: 'controller.home',
 
+  toogleMap: function (button) {
+    var itemId = '#' + button.name,
+      cmp = this.getView().down(itemId),
+      container = (button.name === 'map3' || button.name === 'map4');
+    if (cmp.hidden) {
+      cmp.show();
+      if (container) {
+        var cmpContainer = cmp.up();
+        cmpContainer.show();
+        cmpContainer.activeItems = cmpContainer.activeItems + 1;
+      }
+    }
+    else {
+      cmp.hide();
+      if (container) {
+        var cmpContainer = cmp.up();
+        cmpContainer.activeItems = cmpContainer.activeItems - 1;
+        if (cmpContainer.activeItems === 0) {
+          cmpContainer.hide();
+        }
+      }
+    }
+
+  }
 });
